1376f92255cd26b364517509d2b428fcc08bbad8
Mohamed Hosni <92038848+Eng-MohamedHosni@users.noreply.github.com>
2025-06-21 12:58:53 +0300

    Add files via upload

---
+++ Prefix/PrefixSum base case.cpp
@@ -0,0 +1,36 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+#define endl "\n"
+#define all(v) ((v).begin()),((v).end())
+typedef	long long ll;
+using namespace std;
+
+int dx8[8] = { 1, -1, 0, 0, 1, 1, -1, -1 };
+int dy8[8] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+
+void file() {
+#ifndef ONLINE_JUDGE
+    freopen("in.txt", "r", stdin);//freopen("in.txt", "r", stdin);
+#else 
+#endif
+}
+void fast()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
+
+
+
+int main()
+{
+    fast();
+    ll n, q; cin >> n >> q;
+    vector<ll> prefix(n + 1] = {};
+
+
+    return 0;
+}
+
